0522484e739a0b0e55e98fb0e5a1063e53f9d7f8
ntakashi <nicolas.tcs@hotmail.com>
2017-06-24 16:25:23 +0100

    adicionado componente de loading

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import Loading from '../loading/loading.component';
 
 const Header = props => (
     <nav>
@@ -8,6 +9,7 @@ const Header = props => (
         <IndexLink to="/courses" activeClassName="active">Courses</IndexLink>
         {" | "}
         <IndexLink to="/about" activeClassName="active">About</IndexLink>
+        <Loading interval={100} dots={20} />
     </nav>
 );
 
